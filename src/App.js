a40ef74e5542f6324d624111d27e3f08a312ba17
Davuthan İşçi <hoidxdev@gmail.com>
2024-05-26 19:46:35 +0300

    Clean up, changes image, white spaces handled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import GameComponent from "./components/GameComponent";
-import IframeFood from "./components/iframeFood/IframeFood";
 import NewComponent from "./components/newComponent";
 
 function App() {
   return (
     <div className="App">
-      {/* <IframeFood /> */}
-      {/* <GameComponent/> */}
       <NewComponent/>
     </div>
   );
--- src/components/newComponent.jsx
+++ src/components/newComponent.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { Bodies, Engine, Render, Runner, World, Mouse, MouseConstraint, Events } from 'matter-js';
+import Matter, { Bodies, Engine, Render, Runner, World, Mouse, MouseConstraint, Events } from 'matter-js';
 import { foodList } from './utils/data/foodList';
 
 function NewComponent(props) {
@@ -19,6 +19,13 @@ function NewComponent(props) {
     wallCategory: 3,
   });
 
+  const scalePathData = (pathData, scale) => {
+    return pathData.split(' ').map(coord => (parseFloat(coord) * scale).toFixed(2)).join(' ');
+  };
+  const scaledPathData = scalePathData("136 7 202 24 244 108 305 115 289 144 226 144 190 171 118 158 7 66", 0.8)
+
+  console.log(scaledPathData)
+  
   const addNewFood = (x, y) => {
     if (!isDragging) {  // Only add new food if not dragging
       const foodConfig = foodList[sceneData.mode][Math.floor(Math.random() * foodList[sceneData.mode].length)];
@@ -33,12 +40,21 @@ function NewComponent(props) {
             yScale: scale,
           }
         },
-        density: 0.005,
-        friction: 0.5,
-        frictionAir: 0.025,
-        restitution: 0.7,
-        mass: 10,
+        // density: 0.01,  // Increased for more realistic mass
+        // friction: 0.2,   // Adjusted for more realistic sliding behavior
+        // frictionAir: 0.01, // Reduced for less air resistance
+        // restitution: 0.7, // Adjusted for more realistic bounciness
+        // chamfer: { radius: 50 }, // Adds rounded corners
+
       });
+
+          // Add pathData to define the shape
+    if (foodConfig.pathData) {
+      const path = foodConfig.pathData;
+      const vertices = Matter.Vertices.fromPath(path);
+      Matter.Body.setVertices(newFood, vertices);
+    }
+      
       World.add(engine.current.world, [newFood]);
     }
   };
@@ -60,24 +76,24 @@ function NewComponent(props) {
       options: {
         width: cw,
         height: ch,
-        wireframes: false,
+        wireframes:false,
         background: '#fffafa',
       }
     });
 
     // Add boundaries
     World.add(engine.current.world, [
-      Bodies.rectangle(window.innerWidth * 0.5, window.innerHeight * 0.85, 1000 * 1.1, 50 * 1, {
+      Bodies.rectangle(window.innerWidth * 0.5, window.innerHeight * 0.85, 1000 * 1.6, 50 * 3.2,{
         isStatic: false,
         render: {
           sprite: {
-            texture: "/foods-2/cutting-board.webp",
-            xScale: 0.5,
-            yScale: 0.5
+            texture: "https://res.cloudinary.com/dizm8txou/image/upload/v1716066209/about-us/foods-2/plate-mid_ut7say.webp",
+            xScale: 1.62,
+            yScale: 1.62
           },
         }
       }),
-      Bodies.rectangle(window.innerWidth * 0.5, ch, 1000 * 1, 50, { // Position it at the bottom of the canvas
+      Bodies.rectangle(window.innerWidth * 0.55, ch, 1000 * 1.4, 5,  { // Position it at the bottom of the canvas
         isStatic: true,
         render: {
           visible: false // Make the floor invisible
